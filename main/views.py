9daf0f3e85050697ab8d7dfcc8336ca168efc365
naamiraa <naamiraaulia@gmail.com>
2024-09-11 02:38:53 +0700

    Memperbarui nama aplikasi dan nama saya

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'nama_app : HERITAGE CRAFT'
+        'nama_app' : 'HERITAGE CRAFT',
         'npm' : '2306165931',
         'name': 'Namira Aulia',
         'class': 'PBP C'
