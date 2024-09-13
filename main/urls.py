0ba87c5d739b1e98e5a38041b7cdcc18f6b1a896
naamiraa <naamiraaulia@gmail.com>
2024-09-14 06:57:18 +0700

    Mengembalikan data dalam bentuk XML, JSON, XML by ID, JSON by ID

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from main import views
-from main.views import show_main, create_product_entry
+from main.views import show_main, create_product_entry, show_xml, show_json, show_xml_by_id, show_json_by_id
+
 
 app_name = 'main'
 
@@ -8,4 +9,9 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('', views.show_main, name='show_main'),
     path('create-product-entry', create_product_entry, name='create_product_entry'),
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+
 ]
