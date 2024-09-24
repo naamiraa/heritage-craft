1891edd2fe429b13a73960ab9e07dc73274c4c78
naamiraa <naamiraaulia@gmail.com>
2024-09-24 22:58:45 +0700

    Mengimplementasikan autentikasi, session, dan cookies

--- heritage_craft/settings.py
+++ heritage_craft/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ddp%zwg%9hq@2ns5apm)aox5zl50xrl-=pyx7trt%d%_&6cbnp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "namira-aulia31-heritagecraft.pbp.cs.ui.ac.id"]
 
--- main/models.py
+++ main/models.py
@@ -1,13 +1,15 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
-    image = models.CharField(max_length=255, default='images/default.avif') # Gambar produk
+    image = models.TextField(default='https://private-user-images.githubusercontent.com/178132521/369594940-09a367c0-0869-411b-ba75-36b21d9e21b5.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjcxODk2MDMsIm5iZiI6MTcyNzE4OTMwMywicGF0aCI6Ii8xNzgxMzI1MjEvMzY5NTk0OTQwLTA5YTM2N2MwLTA4NjktNDExYi1iYTc1LTM2YjIxZDllMjFiNS5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwOTI0JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDkyNFQxNDQ4MjNaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02ZTExMTdmYTVkMDEzZGY0M2E4NWUyM2EwZGFiOTBmODJiMzE1Y2M1NzdmZTQ4NjQ4ODJmZDk3YjFkNjdjMWVjJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.EAR_fR-_8DooRA-p0PTyiAzKRKQFQmTsxukcNmxVJg4') # Gambar produk
     category = models.CharField(max_length=100) # Kain batik, ulos, tenun
     place_of_origin = models.CharField(max_length=100) # daerah asal
     stock = models.IntegerField() # Jumlah stok barang
